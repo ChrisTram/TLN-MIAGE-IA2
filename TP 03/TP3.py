73354ea86f848c2a81d786df756eee1c331ff0ff
Chris TRAMIER <chris.tramier97@gmail.com>
2020-10-08 09:03:11 +0200

    stem_prefix

--- TP 03/TP3.py
+++ TP 03/TP3.py
@@ -2,7 +2,9 @@ import nltk
 from nltk import ne_chunk, pos_tag
 from nltk.tokenize import word_tokenize
 import re
-
+from nltk.corpus import words
+from nltk.corpus import wordnet as wn
+from nltk.stem import PorterStemmer
 
 def preprocessing(text):
     tokenize_text = word_tokenize(text)
@@ -44,7 +46,7 @@ def switch_question_type(argument):
         "who": ["person", "org"],
         "whose": [""],
         "when": ["time"],
-        "which": [""]
+        "which": [""] #Prendre le mot qui vient après
     }
     return switcher.get(argument, None)
 
@@ -63,6 +65,51 @@ def get_response_type(text):
 
     return responses
 
+
+# From https://dictionary.cambridge.org/grammar/british-grammar/word-formation/prefixes
+english_prefixes = {
+"anti": "",    # e.g. anti-goverment, anti-racist, anti-war
+"auto": "",    # e.g. autobiography, automobile
+"de": "",      # e.g. de-classify, decontaminate, demotivate
+"dis": "",     # e.g. disagree, displeasure, disqualify
+"down": "",    # e.g. downgrade, downhearted
+"extra": "",   # e.g. extraordinary, extraterrestrial
+"hyper": "",   # e.g. hyperactive, hypertension
+"il": "",     # e.g. illegal
+"im": "",     # e.g. impossible
+"in": "",     # e.g. insecure
+"ir": "",     # e.g. irregular
+"inter": "",  # e.g. interactive, international
+"mega": "",   # e.g. megabyte, mega-deal, megaton
+"mid": "",    # e.g. midday, midnight, mid-October
+"mis": "",    # e.g. misaligned, mislead, misspelt
+"non": "",    # e.g. non-payment, non-smoking
+"over": "",  # e.g. overcook, overcharge, overrate
+"out": "",    # e.g. outdo, out-perform, outrun
+"post": "",   # e.g. post-election, post-warn
+"pre": "",    # e.g. prehistoric, pre-war
+"pro": "",    # e.g. pro-communist, pro-democracy
+"re": "",     # e.g. reconsider, redo, rewrite
+"semi": "",   # e.g. semicircle, semi-retired
+"sub": "",    # e.g. submarine, sub-Saharan
+"super": "",   # e.g. super-hero, supermodel
+"tele": "",    # e.g. television, telephathic
+"trans": "",   # e.g. transatlantic, transfer
+"ultra": "",   # e.g. ultra-compact, ultrasound
+"un": "",      # e.g. under-cook, underestimate
+"up": "",      # e.g. upgrade, uphill
+}
+
+
+def stem_prefix(word, prefixes, roots):
+    original_word = word
+    for prefix in sorted(prefixes, key=len, reverse=True):
+        # Use subn to track the no. of substitution made.
+        # Allow dash in between prefix and root. 
+        word, nsub = re.subn("{}[\-]?".format(prefix), "", word)
+        if nsub > 0 and word in roots:
+            return word
+    return original_word
 
 # text = "Which river does the Brooklyn Bridge cross?"
 text = "Who created Wikipedia?"
